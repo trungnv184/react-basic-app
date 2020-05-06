0f93c984692b81ba6e35504b631520d975c074ed
Trung Nguyen <trungnv184@gmail.com>
2020-05-06 21:02:37 +0700

    config debuger

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import reducer from "./store/reducer";
 
-const store = createStore(reducer);
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("[Middleware] Dispatching", action);
+      const result = next(action);
+
+      console.log("[Middleware] next state", store.getState());
+
+      return result;
+    };
+  };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(
   <Provider store={store}>
